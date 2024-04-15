37931dcf83d0c50911bef4d58a1fab7b84a813ea
Jon Hilton <hilton.jon@gmail.com>
2024-04-15 09:36:34 +0100

    Initial commit

---
+++ BlazorPlayground/Components/FipsDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorPlayground.Components;
+
+public record FipsDetail
+{
+    
+};
+
+public class FipsDetailQuery
+{
+}
+
+public class FipsService
+{
+    public async Task<FipsDetail?> GetFipsDetail(FipsDetailQuery query)
+    {
+        return new FipsDetail();
+    }
+}
